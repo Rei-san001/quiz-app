03c58ad3c24b612e2874a579788e2c54898015b0
179292160 <Rei-san001@users.noreply.github.com>
2026-01-06 19:53:47 +0600

    start_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Scaffold(
-      body: Center(
-        child: Text('Hello'),
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+          body: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color(0xFF283E51),
+                    Color(0xFF4B79A1),
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+            ),
+            child: const StartScreen(),
+          ),
       ),
     ),
-  ));
+  );
 }
