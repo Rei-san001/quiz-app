074465e545656ad3e1cd0f9ffd3b8df7d745f71b
179292160 <Rei-san001@users.noreply.github.com>
2026-05-20 00:17:42 +0600

    qna

---
+++ lib/answer_button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget{
+  const AnswerButton({
+      super.key,
+      required this.answerText,
+      required this.onSelected,
+
+      }); /*
+      required = mandatory
+      Without it, the value can be skipped
+      Flutter uses it to prevent mistakes
+      */
+
+  //properties
+  final String answerText;
+  final void Function() onSelected;
+
+  //methods
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+
+      onPressed: onSelected,
+      style: ElevatedButton.styleFrom(
+
+        padding: const EdgeInsets.symmetric(
+            horizontal: 40, vertical: 10
+        ),
+        backgroundColor: const Color.fromARGB(255, 18, 43, 100),
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(40), //radius = 40 px
+        ),
+      ),
+      child: Text(answerText),
+    );
+  }
+}
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'answer_button.dart';
+import 'data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -12,31 +14,31 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
+
+    final currentQuestion = questions[0];
      return SizedBox(
        width: double.infinity,  //means as much as width as possible
        child: Column(
          mainAxisAlignment: MainAxisAlignment.center,
-         children: [
 
-         const Text('The Question...'),
+         children: [
+         Text(
+             currentQuestion.text,
+             style: const TextStyle(
+             color: Colors.white
+             )
+         ),
 
          const SizedBox(height: 30),
 
-         ElevatedButton(
-             onPressed: () {},
-             child: const Text('Answer 1'),
-         ),
 
-         ElevatedButton(
-           onPressed: () {},
-           child: const Text('Answer 2'),
-         ),
-
-         ElevatedButton(
-           onPressed: () {},
-           child: const Text('Answer 3'),
-         ),
-       ],),
+           //... simply means it will take all the values from the list and put it in the column
+           //map allows us to convert, to transform, u could say, the values in a list to other values
+           ...currentQuestion.answers.map((answer){
+             return AnswerButton(answerText: answer, onSelected: (){});
+           })
+       ],
+       ),
      );
   }
 }
